dcaa827eab4ac0c658e1a2f148d6468284104cad
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2017-11-22 00:43:23 +0200

    adding automated testing for Collector

--- tst/test_collector.py
+++ tst/test_collector.py
@@ -3,30 +3,26 @@ from main_module import Collector
 
 class TestCollector(unittest.TestCase):
 
-     def test_read_success_percent(self):
+     def test_collect_success_percent(self):
           collector = Collector(["--persuccess"],input_file="../data/test_file", output_file="../data/report1.txt")
-          options =  collector.parser.options
           collector.collect_statistics()
 
-     def test_read_success_percent(self):
+     def test_collect_fail_percent(self):
           collector = Collector(["--perfail"],input_file="../data/test_file", output_file="../data/report2.txt")
-          options =  collector.parser.options
           collector.collect_statistics()
-     """
-     def test_read_success_percent(self):
-          collector = Collector(["--top10"])
+
+     def test_collect_top10requests(self):
+          collector = Collector(["--top10"],input_file="../data/test_file", output_file="../data/report3.txt")
           options =  collector.parser.options
           collector.collect_statistics()
 
-     def test_read_success_percent(self):
-          collector = Collector(["--top10fail"])
-          options =  collector.parser.options
+     def test_top10fail(self):
+          collector = Collector(["--top10fail"],input_file="../data/test_file", output_file="../data/report4.txt")
           collector.collect_statistics()
 
-     def test_read_success_percent(self):
-          collector = Collector(["--top10hosts"])
-          options =  collector.parser.options
+     def test_top10hosts(self):
+          collector = Collector(["--top10hosts"],input_file="../data/test_file", output_file="../data/report5.txt")
           collector.collect_statistics()
-     """
+
 if __name__ == '__main__':
     unittest.main()
