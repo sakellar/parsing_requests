447939c19380a864fc037c4578342a2673a5b3cc
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2017-11-22 00:49:56 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='parsing_requests',
+   version='1.0',
+   description='Parsing utis for HTTP NASA',
+   author='Angelos Sakellaropoulos',
+   author_email='angsakel@gmail.com',
+   python_requires='==2.7',
+   install_requires=['mock'],
+)
