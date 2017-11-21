1e735778bf771ccd4e9cf568ea5e2008514b5bc0
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2017-11-22 00:42:55 +0200

    adding parser.py

--- src/parser.py
+++ src/parser.py
@@ -2,31 +2,32 @@ from optparse import OptionParser
 import logging
 
 class Parser:
+    """Parser Class which initializes parser with provided options"""
     def __init__(self, args):
+        """Parser class __init__ """
         self.parser = self._initialize_parser()
         (self.options, self.args) = self.parser.parse_args(args)
         self._check_options()
         self._check_args()
 
     def _initialize_parser(self):
-        """Initializes Options"""
-        parser = OptionParser("exec [options]")
+        """Initializes Parser"""
+        parser = OptionParser("exec [--top10] [--persucess] [--perfail] [--top10fail] [--top10hosts]")
         parser.add_option("--top10", action="store_true", dest="top10",
-                      help="1.	Top 10 requested pages and the number of requests made for each")
+                      help="1.  Top 10 requested pages and requests for each")
         parser.add_option("--persuccess", action="store_true", dest="persuccess",
-                      help="2.	Percentage of successful requests (anything in the 200s and 300s range)")
+                      help="2.  Percentage of successful requests")
         parser.add_option("--perfail", action="store_true", dest="perfail",
-                      help="3.	Percentage of unsuccessful requests (anything that is not in the 200s or 300s range)")
+                      help="3.  Percentage of unsuccessful requests")
         parser.add_option("--top10fail", action="store_true", dest="top10fail",
-                      help="4.	Top 10 unsuccessful page requests")
+                      help="4.  Top 10 unsuccessful page requests")
         parser.add_option("--top10hosts", action="store_true", dest="top10hosts",
-                      help="5   The top 10 hosts making the most requests, displaying the IP address and number of requests made")
+                      help="5.  The top 10 hosts making the most requests")
         return parser
 
     def _check_options(self):
         """
-            Counts the number of options against 1
-            Logs error a
+        Counts the number of options against 1
         """
         option_counter = 0
 
@@ -53,5 +54,5 @@ class Parser:
     def _check_args(self):
         """Checks number of args"""
         if len(self.args) != 0:
-            parser.error("no arguments needed")
+            self.parser.error("no arguments needed")
 
